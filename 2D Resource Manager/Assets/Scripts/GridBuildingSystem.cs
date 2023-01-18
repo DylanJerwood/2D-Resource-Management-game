f03f09872a0b0806e9d17469cc7785be7d358e69
Dylan Jerwood <104373836+DylanJerwood@users.noreply.github.com>
2023-01-18 15:22:16 +0000

    Further adding to the grid building system

--- 2D Resource Manager/Assets/Scripts/GridBuildingSystem.cs
+++ 2D Resource Manager/Assets/Scripts/GridBuildingSystem.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class GridBuildingSystem : MonoBehaviour
 {
-
+    [SerializeField] private PlacedObjectTypeSO placedObjectTypeSO;
     private Grid<GridObject> grid;
 
     private void Awake() {
-        int gridWidth = 10;
-        int gridHeight = 10;
-        float cellSize = 10f;
+        int gridWidth = 20;
+        int gridHeight = 20;
+        float cellSize = 3f;
         grid = new Grid<GridObject>(gridWidth, gridHeight, cellSize, new Vector3(0, 0, 0), (Grid<GridObject> g, int x, int y) => new GridObject(g, x, y));
     }
 
@@ -18,12 +19,53 @@ public class GridBuildingSystem : MonoBehaviour
 
         private Grid<GridObject> grid;
         private int x;
-        private int z;
+        private int y;
+        private Transform transform;
 
-        public GridObject(Grid<GridObject> grid, int x, int z) {
+        public GridObject(Grid<GridObject> grid, int x, int y) {
             this.grid = grid;
             this.x = x;
-            this .z = z;
+            this .y = y;
+        }
+
+        public void SetTransform(Transform transform) {
+            this.transform = transform;
+            grid.TriggerGridObjectChanged(x,y);
+        }
+
+        public void ClearTransform(Transform transform) {
+            transform = null;
+            grid.TriggerGridObjectChanged(x,y);
+        }
+
+        public bool CanBuild() {
+            return transform == null;
+        }
+
+        public override string ToString() {
+            return x + "," + y + "/n" + transform;
+        }
+        
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            grid.GetXY(UtilsClass.GetMouseWorldPosition(), out int x, out int y);
+
+            List<Vector2Int> gridPositionList = placedObjectTypeSO.GetGridPositionList(new Vector2Int(x,y), PlacedObjectTypeSO.Dir.Down);
+
+            GridObject gridObject = grid.GetGridObject(x,y);
+            if (gridObject.CanBuild()) {
+                Transform buildTransform = Instantiate(placedObjectTypeSO.prefab, grid.GetWorldPosition(x,y), Quaternion.identity);
+
+                foreach(Vector2Int gridPosition in gridPositionList) {
+                    grid.GetGridObject(gridPosition.x, gridPosition.y).SetTransform(buildTransform);
+                }
+                gridObject.SetTransform(buildTransform); 
+            }
+            else {
+                UtilsClass.CreateWorldTextPopup("Cannot build here!", UtilsClass.GetMouseWorldPosition());
+            }
         }
     }
 }
