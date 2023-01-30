3bef82b403d743b825dd3cc9e1f1a2d01280b16e
Dylan Jerwood <104373836+DylanJerwood@users.noreply.github.com>
2023-01-30 15:37:35 +0000

    Added notes explaining scripts
    Added notes explaining the GridBuildingSystem script and the BuildingGhost script

--- 2D Resource Manager/Assets/Scripts/Grid Systems/BuildingGhost.cs
+++ 2D Resource Manager/Assets/Scripts/Grid Systems/BuildingGhost.cs
@@ -5,55 +5,65 @@ using CodeMonkey.Utils;
 
 public class BuildingGhost : MonoBehaviour
 {
+    //References the gridBuildingSystem script
     public GameObject GBS;
     private GridBuildingSystem gridBuildingSystem;
 
-    [SerializeField] public List<GameObject> visualsList;
+    //creates a list of all of the visual prefabs
+    public List<GameObject> visualsList;
+
+    //Variables
     public GameObject visual;
     public bool createPlacementIndicator;
     private GameObject indicator;
     private GameObject middleMan;
     
-
+    //Start Script
     void Awake()
     {
+        //calls gridBuildingSystem Script and stores it in a variable
         gridBuildingSystem = GBS.GetComponent<GridBuildingSystem>();
     }
 
 
     void Update()
     {
+        //Checks is a building has been selected and that the player is placing an object
         if (gridBuildingSystem.placedObjectTypeSO != null && gridBuildingSystem.placingObject == true) {
+            //Checks to see if a placement indicator has already been made
             if(createPlacementIndicator == true) {
+                //If one has not been made it Instantiates one 
                 middleMan = Instantiate(visual, new Vector3(0, 0, 0), Quaternion.identity);
                 indicator = middleMan;
 
+                //changes bool so you only make 1 indicator
                 createPlacementIndicator = false;
             }
         }
 
+        //Checks to see if you press right click (if you do if then removes the indicator object and stores null in the variable)
         if (Input.GetMouseButtonDown(1)) {
             visual = null;
             Destroy(indicator);
         }
 
+        //Player selects building using 1-4    checks for already existing indicator and destroys it
         if (Input.GetKeyDown(KeyCode.Alpha1)) {if (indicator != null) {Destroy(indicator);} visual = visualsList[0]; createPlacementIndicator = true; }
         if (Input.GetKeyDown(KeyCode.Alpha2)) {if (indicator != null) {Destroy(indicator);} visual = visualsList[1]; createPlacementIndicator = true; }
         if (Input.GetKeyDown(KeyCode.Alpha3)) {if (indicator != null) {Destroy(indicator);} visual = visualsList[2]; createPlacementIndicator = true; }
         if (Input.GetKeyDown(KeyCode.Alpha4)) {if (indicator != null) {Destroy(indicator);} visual = visualsList[3]; createPlacementIndicator = true; }
     }
 
+    //indicator movement
     void LateUpdate() {
+        //checks if building is selected
         if(indicator != null) {
+            //moves indicator to your mouse location thats snapped to the grid
             indicator.transform.position = gridBuildingSystem.GetMouseWorldSnappedPosition();
 
+            //When you rotate the building this also rotates the indicator so you can see
             indicator.transform.rotation = Quaternion.Lerp(indicator.transform.rotation, gridBuildingSystem.GetPlacedObjectRotation(), Time.deltaTime * 15f);
         }
         
     }
-
-    //what do i need to happen
-
-    //change rotation of indicator
-    //change colour depending on weather they can build or not
 }
--- 2D Resource Manager/Assets/Scripts/Grid Systems/GridBuildingSystem.cs
+++ 2D Resource Manager/Assets/Scripts/Grid Systems/GridBuildingSystem.cs
@@ -6,25 +6,32 @@ using CodeMonkey.Utils;
 
 public class GridBuildingSystem : MonoBehaviour
 {
-    [SerializeField] public List<PlacedObjectTypeSO> placedObjectTypeSOList;
+    //List for buildings
+    public List<PlacedObjectTypeSO> placedObjectTypeSOList;
+    //Variable for selected building
     public PlacedObjectTypeSO placedObjectTypeSO;
 
+    //variables to set up grid
     private Grid<GridObject> grid;
     private PlacedObjectTypeSO.Dir dir = PlacedObjectTypeSO.Dir.Down;
     private int gridWidth = 133;
     private int gridHeight = 133;
     private float cellSize = 1.5f;
+
+    //variable to check if the player is currently placing a building
     public bool placingObject;
 
     private void Awake() {
 
+        //On awake this creates the gird using the public class GridObject
         grid = new Grid<GridObject>(gridWidth, gridHeight, cellSize, new Vector3(-100, -100, 0), (Grid<GridObject> g, int x, int y) => new GridObject(g, x, y));
 
+        //readies variables
         placedObjectTypeSO = null;
-
         placingObject = false;
     }
 
+    //Stores information and subroutines for grid functions 
     public class GridObject {
 
         private Grid<GridObject> grid;
@@ -32,30 +39,36 @@ public class GridBuildingSystem : MonoBehaviour
         private int y;
         private PlacedObject placedObject;
 
+        //gets x and y positions on the grid
         public GridObject(Grid<GridObject> grid, int x, int y) {
             this.grid = grid;
             this.x = x;
-            this .y = y;
+            this.y = y;
         }
 
+        //Changes grid value to the placed object
         public void SetPlacedObject(PlacedObject placedObject) {
             this.placedObject = placedObject;
             grid.TriggerGridObjectChanged(x,y);
         }
 
+        //function to check the placed object in the grid space
         public PlacedObject GetPlacedObject() {
             return placedObject;
         }
 
+        //function to clear grid space
         public void ClearPlacedObject() {
             placedObject = null;
             grid.TriggerGridObjectChanged(x,y);
         }
 
+        //Checks to see if you can build there
         public bool CanBuild() {
             return placedObject == null;
         }
 
+        //Writes string in the grid spaces
         public override string ToString() {
             return "";
         }
@@ -64,16 +77,23 @@ public class GridBuildingSystem : MonoBehaviour
 
     private void Update() {
 
+        //Checks if the player selected a building
         if(placedObjectTypeSO != null) {
 
+            //Checks if player pressed left click
             if(Input.GetMouseButtonDown(0)) {
+                //Converts mouse position to an X and Y variable
                 grid.GetXY(UtilsClass.GetMouseWorldPosition(), out int x, out int y);
 
+                //checks the player has pressed in the bounds of the grid
                 if (x >= 0 && y >= 0 && x < gridWidth && y < gridHeight) {
                     
+                    //Grabs the position for every place one the list
                     List<Vector2Int> gridPositionList = placedObjectTypeSO.GetGridPositionList(new Vector2Int(x,y), dir);
 
+                    //checks if the player can build there
                     bool canBuild = true;
+                    //For every position the object will make it so you cant build there anymore
                     foreach (Vector2Int gridPosition in gridPositionList) {
                         if (!grid.GetGridObject(gridPosition.x, gridPosition.y).CanBuild()) {
                             canBuild = false;
@@ -81,18 +101,23 @@ public class GridBuildingSystem : MonoBehaviour
                         }
                     }
 
+                    //checks if the player can build
                     if (canBuild) {
+                        //Establishes the rotation ofset you would need to place the object correctly
                         Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
+                        //places the object in each spot it will take up
                         Vector3 placedObjectWorldPosition = grid.GetWorldPosition(x, y) + new Vector3(rotationOffset.x, rotationOffset.y) * grid.GetCellSize();
 
+                        //creates the object in the grid
                         PlacedObject placedObject = PlacedObject.Create(placedObjectWorldPosition, new Vector2Int(x,y), dir, placedObjectTypeSO);
+                        //chages its rotation
                         placedObject.transform.rotation = Quaternion.Euler(0, 0, -placedObjectTypeSO.GetRotationAngle(dir));
                         
-
+                        //Changes the value of the grid to placed object
                         foreach(Vector2Int gridPosition in gridPositionList) {
                             grid.GetGridObject(gridPosition.x, gridPosition.y).SetPlacedObject(placedObject);
                         }
-                        
+                    //If player cant build creates text to tell them    
                     }
                     else {
                         UtilsClass.CreateWorldTextPopup("Cannot build here!", UtilsClass.GetMouseWorldPosition());
@@ -103,34 +128,40 @@ public class GridBuildingSystem : MonoBehaviour
                 }
             }
 
+            //checks if player pressed right click
             if (Input.GetMouseButtonDown(1)) {
+                //removes selected building and changes it to nothing
                 placedObjectTypeSO = null;
                 placingObject = false;
             }
         
-            if (Input.GetKeyDown(KeyCode.F)) {
-                GridObject gridObject = grid.GetGridObject(UtilsClass.GetMouseWorldPosition());
-                PlacedObject placedObject = gridObject.GetPlacedObject();
-                if(placedObject != null) {
-                    placedObject.DestroySelf();
-
-                    List<Vector2Int> gridPositionList = placedObject.GetGridPositionList();
-                    
-                    foreach(Vector2Int gridPosition in gridPositionList) {
-                        grid.GetGridObject(gridPosition.x, gridPosition.y).ClearPlacedObject();
-                    }
-                }
-            }
-
+            //check if the player pressed R
             if (Input.GetKeyDown(KeyCode.R)) {
+                //Changes the objects direction
                 dir = PlacedObjectTypeSO.GetNextDir(dir);
+                //Creates text telling player where it rotated
                 UtilsClass.CreateWorldTextPopup("" + dir, UtilsClass.GetMouseWorldPosition());
 
             }
         }
 
+        //checks player pressed F
+        if (Input.GetKeyDown(KeyCode.F)) {
+            //Destroys the object in that grid spot
+            GridObject gridObject = grid.GetGridObject(UtilsClass.GetMouseWorldPosition());
+            PlacedObject placedObject = gridObject.GetPlacedObject();
+            if(placedObject != null) {
+                placedObject.DestroySelf();
 
+                List<Vector2Int> gridPositionList = placedObject.GetGridPositionList();
+    
+                foreach(Vector2Int gridPosition in gridPositionList) {
+                    grid.GetGridObject(gridPosition.x, gridPosition.y).ClearPlacedObject();
+                }
+            }
+        }
 
+        //Player can slect which building to use by pressing 1-4
         if (Input.GetKeyDown(KeyCode.Alpha1)) {placedObjectTypeSO = placedObjectTypeSOList[0]; placingObject = true; }
         if (Input.GetKeyDown(KeyCode.Alpha2)) {placedObjectTypeSO = placedObjectTypeSOList[1]; placingObject = true; }
         if (Input.GetKeyDown(KeyCode.Alpha3)) {placedObjectTypeSO = placedObjectTypeSOList[2]; placingObject = true; }
@@ -139,6 +170,9 @@ public class GridBuildingSystem : MonoBehaviour
 
     }
 
+    //Functions
+
+    //Gets the snapped position for the mouse
     public Vector3 GetMouseWorldSnappedPosition() {
         Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
         grid.GetXY(mousePosition, out int x, out int y);
@@ -152,6 +186,7 @@ public class GridBuildingSystem : MonoBehaviour
         }
     }
 
+    //Gets the placed objects rotation
     public Quaternion GetPlacedObjectRotation() {
         if (placedObjectTypeSO != null) {
             return Quaternion.Euler(0, 0, -placedObjectTypeSO.GetRotationAngle(dir));
