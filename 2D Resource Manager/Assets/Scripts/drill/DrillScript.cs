7f7c1ebe4241661cc9b7df6d4944d2ada3d3a8d3
DylanJerwood <104373836+DylanJerwood@users.noreply.github.com>
2023-02-20 10:09:50 +0000

    Continued drill development

--- 2D Resource Manager/Assets/Scripts/drill/DrillScript.cs
+++ 2D Resource Manager/Assets/Scripts/drill/DrillScript.cs
@@ -5,67 +5,75 @@ using UnityEngine;
 public class DrillScript : MonoBehaviour
 {
     public string drillType;
-    public List<Conveyor> conveyorList;
-    public List<GameObject> raycastList;
+    public GameObject origin;
+    public GameObject box;
+    public Conveyor[] conveyorList;
     public Conveyor conveyor;
+    public ConveyorItem drillItem;
+    private ConveyorItem x;
+    private GameObject itemPosition;
+    private Vector3 position;
+
+    private float nextItemDelivery = 0.0f;
+    public float drillSpeed = 0.5f;
 
     private void Start() {
-
+        conveyorList = GetConveyorList();
     }
 
     private void Update() {
 
-        GetConveyorList();
+        conveyorList = GetConveyorList();
         
+
+        
+        foreach(var con in conveyorList) {
+            conveyor = con;
+            if(Time.time > nextItemDelivery) {
+                nextItemDelivery += drillSpeed;
+                if(conveyor != null) {
+                    if(conveyor.conveyorItem == null) {
+                        itemPosition = conveyor.transform.GetChild(1).gameObject;
+                        position = new Vector3(itemPosition.transform.position.x, itemPosition.transform.position.y, itemPosition.transform.position.z);
+                        x =  Instantiate(drillItem, position, Quaternion.identity);
+                        conveyor.conveyorItem = x;
+                        x.transform.parent = conveyor.transform;
+                    }
+                }
+            }
+        }
     }
 
-    private List<Conveyor> GetConveyorList() {
-
-        for (int x = 0; x < raycastList.Count; x++) {
-            Vector2 origin = new Vector2(raycastList[x].transform.position.x, raycastList[x].transform.position.y);
-
-            Debug.DrawRay(origin, transform.TransformDirection(Vector2.down) * 1f, Color.red);
-            Debug.DrawRay(origin, transform.TransformDirection(Vector2.up) * 1f, Color.red);
-            Debug.DrawRay(origin, transform.TransformDirection(Vector2.left) * 1f, Color.red);
-            Debug.DrawRay(origin, transform.TransformDirection(Vector2.right) * 1f, Color.red);
-            
-            for(int i = 0; i < 3; i++) {
-                if(i==0) {
-                    RaycastHit2D hit = Physics2D.Raycast(origin, transform.TransformDirection(Vector2.down), 1f);
-                    if(hit) {
-                        Conveyor conveyor = hit.collider.GetComponent<Conveyor>();
-                        conveyorList.Add(conveyor);
-                    }
-                    
-                }
-                else if(i==1) {
-                    RaycastHit2D hit = Physics2D.Raycast(origin, transform.TransformDirection(Vector2.up), 1f);
-                    if(hit) {
-                        Debug.Log("hit");
-                        Conveyor conveyor = hit.collider.GetComponent<Conveyor>();
-                        conveyorList.Add(conveyor);
-                    }
-                }
-                else if(i==2) {
-                    RaycastHit2D hit = Physics2D.Raycast(origin, transform.TransformDirection(Vector2.left), 1f);
-                    if(hit) {
-                        Conveyor conveyor = hit.collider.GetComponent<Conveyor>();
-                        conveyorList.Add(conveyor);
-                    }                
-                }
-                else {
-                    RaycastHit2D hit = Physics2D.Raycast(origin, transform.TransformDirection(Vector2.right), 1f);
-                    if(hit) {
-                        Conveyor conveyor = hit.collider.GetComponent<Conveyor>();
-                        conveyorList.Add(conveyor);
-                    }                
-                }
-                
-            }
-            
-            
+    private Conveyor[] GetConveyorList() {
+    
+        Collider2D[] listOfConveyorsColliders = new Collider2D[12];
+        Conveyor[] listOfConveyors = new Conveyor[12];
+        Vector2 boxScale = box.transform.localScale / 2;
+        
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        int i=0;
+        
+        if(drillType == "bigDrill") {
+            listOfConveyorsColliders = new Collider2D[20];
+            listOfConveyors = new Conveyor[20];
         }
-        return conveyorList;
+        else if (drillType == "longDrill"){
+            listOfConveyorsColliders = new Collider2D[20];
+            listOfConveyors = new Conveyor[20];
+        }
+        
+        int numOfConveyors = Physics2D.OverlapBox(origin.transform.position, boxScale, 0f, contactFilter, listOfConveyorsColliders);
+
+
+        foreach(var col in listOfConveyorsColliders) {
+            if(col) {
+                listOfConveyors[i] = col.GetComponent<Collider2D>().GetComponent<Conveyor>();
+            }
+
+            i++;
+        }
+
+        return listOfConveyors;
     }
 
 }
