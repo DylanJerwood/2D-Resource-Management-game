deb59a603b0c463cfb1c1e0e76dbbc8b524a03db
Dylan Jerwood <104373836+DylanJerwood@users.noreply.github.com>
2023-02-27 15:58:42 +0000

    Finished Turret script

--- 2D Resource Manager/Assets/Scripts/Enemy.cs
+++ 2D Resource Manager/Assets/Scripts/Enemy.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{
+{   
+    //Variable for enemy health
     public float health = 100f;
-
+    
     private void Update() {
+        //chacks if its health is less than or equal to zero if it is destroys the enemy
         if (health  <= 0f) {
             Destroy(gameObject);
         }
